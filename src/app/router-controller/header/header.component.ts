95d8265e11127ef14a774d0214d781efd42e0680
AugustoAlencar <augusto.alc1@hotmail.com>
2018-12-09 20:03:00 -0200

    Atualização de routes

--- src/app/router-controller/header/header.component.ts
+++ src/app/router-controller/header/header.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Router,
+   Event as RouterEvent,
+  NavigationStart,
+  NavigationEnd,
+  NavigationCancel,
+  NavigationError
+} from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +18,21 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+    navigationInterceptor(event: RouterEvent): void {
+    if (event instanceof NavigationStart) {
+      /* this.loading = true; */
+    }
+    if (event instanceof NavigationEnd) {
+      /* this.loading = false; */
+    }
+    if (event instanceof NavigationCancel) {
+      /* this.loading = false; */
+    }
+    if (event instanceof NavigationError) {
+      /* this.loading = false; */
+    }
   }
 
 }
