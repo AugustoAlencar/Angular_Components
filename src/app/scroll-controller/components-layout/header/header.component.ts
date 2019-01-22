212de503dda455a0287834db5e23f035bdd63d84
AugustoAlencar <augusto.alc1@hotmail.com>
2019-01-22 03:17:12 -0200

    criação do header e conteudos para realização do teste

--- src/app/scroll-controller/components-layout/header/header.component.ts
+++ src/app/scroll-controller/components-layout/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-header',
+  selector: 'app-header-scroll',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+
+  public list: Array<string> = ['Menu 1','Menu 2', 'Menu 3', 'Menu 4'];
 
   constructor() { }
 
--- src/app/scroll-controller/components-layout/list-of-content/list-of-content.component.ts
+++ src/app/scroll-controller/components-layout/list-of-content/list-of-content.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-of-content',
+  selector: 'app-list-of-content-scroll',
   templateUrl: './list-of-content.component.html',
-  styleUrls: ['./list-of-content.component.css']
+  styleUrls: ['./list-of-content.component.scss']
 })
 export class ListOfContentComponent implements OnInit {
+
+  public list: Array<string> = [
+    'Conteudo clicavel numero 1',
+    'Conteudo clicavel numero 2',
+    'Conteudo clicavel numero 3',
+    'Conteudo clicavel numero 4',
+    'Conteudo clicavel numero 5',
+    'Conteudo clicavel numero 6',
+    'Conteudo clicavel numero 7',
+  ]
 
   constructor() { }
 
